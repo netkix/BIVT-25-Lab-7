ec83b7401105649511ce297aee313a7f4b472a2f
netkix <163342764+netkix@users.noreply.github.com>
2026-03-02 21:57:57 +0300

    Update Task1.cs

--- Lab7/Blue/Task1.cs
+++ Lab7/Blue/Task1.cs
@@ -1,6 +1,52 @@
-﻿namespace Lab7.Blue
+namespace Lab7.Blue
 {
     public class Task1
     {
+        public struct Response
+        {
+            // Создание приватных полей
+            private string _name;
+            private string _surname;
+            private int _votes;
+            
+            // Публичные свойства для полей
+            public string Name => _name;
+            public string Surname => _surname;
+            public int Votes => _votes;
+
+            // Конструктор только для name and surname
+            public Response(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+            }
+
+            public int CountVotes(Response[] responses)
+            {
+                int count = 0;
+                for (int i = 0; i < responses.Length; i++)
+                {
+                    if (_name == responses[i]._name && _surname == responses[i]._surname)
+                    {
+                        count++;
+                    }
+                }
+                
+                for (int i = 0; i < responses.Length; i++)
+                {
+                    if (_name == responses[i]._name && _surname == responses[i]._surname)
+                    {
+                        responses[i]._votes = count;
+                    }
+                }
+                
+                return count;
+            }
+            
+            public void Print()
+            {
+                Console.WriteLine("Name: " + _name + ", Surname: " + _surname + ", Votes: " + _votes);
+            }
+        }
     }
 }
